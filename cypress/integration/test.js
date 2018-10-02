f38e0069735b36dbca64b9db6d05aa1485eae348
Solo244 <solo244@gmail.com>
2018-10-02 11:14:45 +0200

    Testing adding Cypress as testing framework

---
+++ cypress/integration/test.js
@@ -0,0 +1,9 @@
+//var pkg = require("/package.json");
+//var $url = pkg.url.local;
+
+describe('Server up', function() {
+  it('See if your server is running', function() {
+    cy.visit('/')
+    //cy.contains('type')
+  })
+})
